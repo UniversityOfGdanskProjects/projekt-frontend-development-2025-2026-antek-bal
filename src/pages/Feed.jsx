880c0a28752c9d1c105cd24339415bdd5763aadb
Antoni Balcerzak <antekbal06@gmail.com>
2025-12-08 13:23:32 +0100

    Added posts to the feed (wall)

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,25 @@
+import {users, posts} from "../data/mockData"
+import PostCard from "../components/PostCard"
+
 function Feed() {
     return (
         <div>
             <h1>Wall</h1>
+            <div className="feed-posts">
+                {posts.map(post => {
+                    const author = users.find(user => user.id === post.author)
+
+                    return (
+                        <PostCard
+                            key={post.id}
+                            post={post}
+                            author={author}
+                        />
+                    )
+                })}
+            </div>
         </div>
     )
 }
+
 export default Feed;
