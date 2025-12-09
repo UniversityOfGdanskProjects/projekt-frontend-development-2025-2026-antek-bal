098fa6a5825a273fae55ed5548f1b14a45136e38
Antoni Balcerzak <antekbal06@gmail.com>
2025-12-09 19:48:19 +0100

    Feed filtring completed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,7 +3,10 @@ import PostCard from "../components/PostCard"
 import './Feed.scss'
 
 function Feed() {
-    const sortedPosts = [...posts].sort((a, b) => new Date(b.date) - new Date(a.date))
+    const CURRENT_USER_ID = 1;
+    const currentUser = users.find(u => u.id === CURRENT_USER_ID);
+    const filteredPosts = posts.filter(p => p.visibility === "public" || (p.visibility === "friends" && currentUser.friends.includes(p.author)) || p.author === CURRENT_USER_ID);
+    const sortedPosts = [...filteredPosts].sort((a, b) => new Date(b.date) - new Date(a.date))
 
     return (
         <div className="feed-page">
