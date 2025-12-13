aeb50fea5c80975143fb4f294e81f0bb7a801a0c
Antoni Balcerzak <antekbal06@gmail.com>
2025-12-14 00:30:09 +0100

    Added functions in authcontext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,80 @@
-import {users} from '../data/mockData.js'
-import {useState} from "react";
+import {useState, createContext, useContext, useEffect} from "react";
+import {users} from '../data/mockData.js';
 
-function AuthContext() {
+const AuthContext = createContext(null)
+
+export const AuthProvider = ({children}) => {
     const [allUsers, updateUsers] = useState(() => {
         const savedUsers = localStorage.getItem("users");
-
         return savedUsers ? JSON.parse(savedUsers) : users;
     })
+
     const [currentUser, setCurrentUser] = useState(() => {
-        const u = localStorage.getItem("current-user");
-        return u ? JSON.parse(u) : null;
+        const user = localStorage.getItem("current-user");
+        return user ? JSON.parse(user) : null;
     });
+
+    useEffect(() => {
+        localStorage.setItem("users", JSON.stringify(allUsers));
+    }, [allUsers]);
+
+    useEffect(() => {
+        if (currentUser) {
+            localStorage.setItem("current-user", JSON.stringify(currentUser));
+        } else {
+            localStorage.removeItem("current-user");
+        }
+    }, [currentUser]);
+
+    const login = (username, password) => {
+        const passwordHash = btoa(password)
+        const user = allUsers.find(u => u.username === username && u.password === passwordHash)
+        if (user) {
+            setCurrentUser(user);
+            return true;
+        }
+        return false;
+    }
+
+    const register = (username, password, name, surname, avatar="https://www.gravatar.com/avatar/?d=mp&s=256") => {
+        const usernameOccupied = allUsers.find(u => u.username === username);
+        if (usernameOccupied) {
+            return false
+        }
+
+        const newUser = {
+            "id": Date.now(),
+            "username": username,
+            "password": btoa(password),
+            "name": name,
+            "surname": surname,
+            "avatar": avatar,
+            "friends": []
+        };
+
+       updateUsers([...allUsers, newUser]);
+       setCurrentUser(newUser);
+       return true;
+    }
+
+    const logout = () => {
+        setCurrentUser(null);
+        return true
+    }
+
+    const value = {
+        allUsers,
+        currentUser,
+        login,
+        logout,
+        register
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
-export default AuthContext;
+export const useAuth = () => useContext(AuthContext);
