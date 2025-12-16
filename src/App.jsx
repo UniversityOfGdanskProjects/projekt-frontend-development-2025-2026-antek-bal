fc134c9eb12d3f198ad7ffbc5be8c260d087be5f
Antoni Balcerzak <antekbal06@gmail.com>
2025-12-16 19:35:32 +0100

    Added protection to profile page and feed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import Layout from "./components/Layout.jsx";
 import Feed from "./pages/Feed.jsx";
 import Profile from "./pages/Profile.jsx";
 import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoutes.jsx";
 
 
 function App() {
     return (
         <Layout>
             <Routes>
-                <Route path="/" element={<Feed/>}></Route>
-                <Route path="/login" element={<Login/>}></Route>
-                <Route path="/profile/:userId" element={<Profile/>}></Route>
+                <Route path="/">
+                    <ProtectedRoute>
+                        <Feed/>
+                    </ProtectedRoute>
+                </Route>
+                <Route path="/login">
+                        <Login/>
+                </Route>
+                <Route path="/profile/:userId">
+                    <ProtectedRoute>
+                        <Profile/>
+                    </ProtectedRoute>
+                </Route>
             </Routes>
         </Layout>
     );
