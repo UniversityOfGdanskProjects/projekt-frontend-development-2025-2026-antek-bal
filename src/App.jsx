c20ed060143f7936114e3b57bc393f224181949b
Antoni Balcerzak <antekbal06@gmail.com>
2025-12-18 18:43:48 +0100

    Updated App to handle admin panel route

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,39 @@ import ProtectedRoute from "./components/ProtectedRoutes.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
 import {AuthProvider} from "./context/AuthContext.jsx";
 import ChatDock from "./components/Chat/ChatDock.jsx";
+import AdminPanel from "./pages/AdminPanel.jsx";
+import AdminRoute from "./components/AdminRoute.jsx";
+import {PostProvider} from "./context/PostContext.jsx";
 
 
 function App() {
     return (
         <AuthProvider>
-            <ChatProvider>
-                <Layout>
-                    <Routes>
-                        <Route path="/" element={
-                            <ProtectedRoute>
-                                <Feed/>
-                            </ProtectedRoute>
-                        }></Route>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/profile/:userId" element={
-                            <ProtectedRoute>
-                                <Profile/>
-                            </ProtectedRoute>
-                        }></Route>
-                    </Routes>
-                </Layout>
-                <ChatDock/>
-            </ChatProvider>
+            <PostProvider>
+                <ChatProvider>
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element={
+                                <ProtectedRoute>
+                                    <Feed/>
+                                </ProtectedRoute>
+                            }></Route>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/profile/:userId" element={
+                                <ProtectedRoute>
+                                    <Profile/>
+                                </ProtectedRoute>
+                            }></Route>
+                            <Route path="/admin" element={
+                                <AdminRoute>
+                                    <AdminPanel/>
+                                </AdminRoute>
+                            }></Route>
+                        </Routes>
+                    </Layout>
+                    <ChatDock/>
+                </ChatProvider>
+            </PostProvider>
         </AuthProvider>
 
     );
